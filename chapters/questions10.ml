2c07a16bcf3a954e3c72d059eb05c8f4b9b2eab5
Emc54 <35582806+Emc54@users.noreply.github.com>
2024-03-31 16:08:59 +0100

    corrected question 4

--- chapters/questions10.ml
+++ chapters/questions10.ml
@@ -1,9 +1,12 @@
+#use "..\\utils.ml";;
+
 (*Question 1*)
 
 type rect =
 LW of int * int
 | SQ of int
 
+let testlist = [SQ 6; LW (4, 3); LW (5, 6); SQ 2];;
 (*Question 2*)
 
 let rectArea rect = 
@@ -27,3 +30,18 @@ let stand rect =
   |SQ _ -> rect
 
 (*Question 4*)
+let rect_sort reclist = sort (map stand reclist)
+ (*This solution doesn't check the squares*)  
+
+(*Q4 Book Solution*)
+
+let width_of_rect r =
+  match r with
+  SQ s -> s
+  | LW (w,_) -> w
+
+let rect_comp a b =
+  width_of_rect a < width_of_rect b
+let pack rects =
+ csort rect_comp (map stand rects)
+ 
